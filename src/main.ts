ad23536c6dabdb6cd06e8177e3cfccbd2bc83172
NadiiaUa23 <hasiminka92@gmail.com>
2025-06-20 18:26:29 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import "./style.css";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const getUserNames = (users: User[]): string[] => {
+  return users.map((user) => user.name);
+};
+
+const userList: User[] = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+];
+
+const names = getUserNames(userList);
+console.log(names); // ['Alice', 'Bob', 'Charlie']
